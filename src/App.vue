c366ceaef0cd8682c33b0f581ae26329ed38da5d
keivn <xiyouji.xiaoyaoyou.kevinChen@gmail.com>
2020-08-07 10:12:56 +0800

    feat: add app.vue init mobile 1px

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,34 @@ export default {
 
 <style lang="scss">
 @import url('/assets/style/index.scss');
+@import url('/assets/style/1px.scss');
+/*1px border 开始*/
+/* 下border */
+.zm-b-b {
+  .zm-1px-b(#eee);
+}
+
+/* 上border */
+.zm-b-t {
+  .zm-1px-t(#eee);
+}
+
+/* 左border */
+.zm-b-l {
+  .zm-1px-l(#eee);
+}
+
+/* 右border */
+.zm-b-r {
+  .zm-1px-r(#eee);
+}
+
+/* 全部border */
+.zm-b {
+  .zm-1px(#eee);
+}
+
+/*1px border 结束*/
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
