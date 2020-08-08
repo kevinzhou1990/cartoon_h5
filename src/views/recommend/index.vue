c0caa02c8968e97b3941fa539be0900e9bb20ec4
keivn <xiyouji.xiaoyaoyou.kevinChen@gmail.com>
2020-08-08 17:04:09 +0800

    feat: add remcommend list change commponents

--- src/views/recommend/index.vue
+++ src/views/recommend/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="box">
-    <z-m-header :title-text="titleText" show-right>
-      <div slot="right" class="right-change" @click="handleClickLightIcon">
-        <img class="right-change-l" :src="showLightBgIcon[0]" alt="">
-        <img class="right-change-r" :src="showLightBgIcon[1]" alt="">
-      </div>
-    </z-m-header>
+    <div style="position: relative;">
+      <z-m-header :title-text="titleText" show-right>
+        <div slot="right" class="right-change">
+          <div class="icon-bg" :class="isLightIcon ? 'trl-l': 'trl-r' "></div>
+        </div>
+      </z-m-header>
+      <div :class="isLightIcon ? 'icon-l-g': 'icon-l-g-l' " @click="handleClickLightIcon"></div>
+      <div :class="isLightIcon ? 'icon-l-g-r': 'icon-r-g' " @click="handleClickLightIcon"></div>
+    </div>
     <div style="height: 1000px; bottom: 0;">
-
+        11111
     </div>
   </div>
 </template>
@@ -31,6 +34,11 @@ export default {
     }
   },
   computed: {
+    /**
+     * @info: 判断显示的图标
+     * @author: PengGeng
+     * @date: 8/8/20-10:12 上午
+     */
     showLightBgIcon() {
       if (this.isLightIcon) {
         return [this.listBb, this.blockBa]
@@ -51,38 +59,85 @@ export default {
 <style lang="scss" scoped>
   $imgUrl: './images/';
   $imgName: block_bb !default;
+  $xLineLength: 3px!default;
   @mixin imgSet($imgName) {
-    box-sizing: border-box;
+    background: url(./images/#{$imgName}.png) no-repeat;
+    background-size: 100%;
+    transition: all 0.6s ease-in-out;
+  }
+  @mixin iconBg {
+    position: fixed;
+    display: block;
+    top: 10px;
     width: 16px;
     height: 16px;
-    padding: 0 2px;
-    background: url(./images/#{$imgName}.png) no-repeat center;
-    background: 100%;
+    margin-top: 4px;
   }
-
+  @mixin trans($xLineLength: 3px) {
+    transition: transform .3s;
+    transform: translateX(#{$xLineLength}px);
+  }
   .right-change {
     display: flex;
-    justify-content: space-between;
-    justify-items: center;
     width: 44px;
     height: 24px;
     line-height: 24px;
     border-radius: 12px;
     margin: 10px 0;
     background: #E6E6E6;
-
-    &-l {
-      width: 16px;
-      height: 16px;
-      margin-top: 4px;
-      margin-left: 6px;
-    }
-
-    &-r {
-      width: 16px;
-      height: 16px;
-      margin-top: 4px;
-      margin-right: 6px;
-    }
   }
+  .icon-bg {
+    margin: 3px 0 3px 0;
+    width: 18px;
+    height: 18px;
+    background: #ffffff;
+    border-radius: 50%;
+    /*&:before {*/
+    /*  background: #ffffff;*/
+    /*  transition: transform .3s;*/
+    /*  transform: translateX(18px);*/
+    /*}*/
+    /*&:after {*/
+    /*  background: #ffffff;*/
+    /*  transition: transform .3s;*/
+    /*  transform: translateX(18px);*/
+    /*}*/
+  }
+  .trl-l {
+    @include trans(22)
+  }
+  .trl-r {
+    @include trans(3)
+  }
+  .icon-l-g {
+    @include iconBg;
+    right: 40px;
+    transition:  all .3s ease-in-out;
+    background: url("./images/list_bb.png") no-repeat;
+    background-size: 100%;
+  }
+  .icon-r-g {
+    @include iconBg;
+    right: 20px;
+    transition:  all .3s ease-in-out;
+    background: url("./images/block_bb.png") no-repeat;
+    background-size: 100%;
+  }
+  .icon-l-g-l {
+    right: 40px;
+    @include iconBg;
+    @include imgSet(list_ba)
+    /*background: url("./images/list_ba.png") no-repeat;*/
+    /*background-size: 100%;*/
+    /*transition: all 0.8s ease-in-out;*/
+  }
+  .icon-l-g-r {
+    right: 20px;
+    @include iconBg;
+    @include imgSet(block_ba)
+    /*background: url("./images/block_ba.png") no-repeat;*/
+    /*background-size: 100%;*/
+    /*transition: all 0.8s ease-in-out;*/
+  }
+
 </style>
