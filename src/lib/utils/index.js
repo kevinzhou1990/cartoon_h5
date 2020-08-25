eccd80690e69d5b3991654c33e96cc5796151349
AgentK <xiyouji.xiaoyaoyou.clx@gmail.com>
2020-08-25 10:48:38 +0800

    feat：add timetamp function, use:  import {timestampToTime} from '@/lib/utils'   timestampToTime(timestamp)

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,9 +1,9 @@
 import crypto from 'crypto-js';
 
 /**
-   * 获取16位随机字符串
-   */
-function getRandomStr (){
+ * 获取16位随机字符串
+ */
+function getRandomStr() {
   return 'xxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     let r = (Math.random() * 16) | 0;
     let v = c === 'x' ? r : (r & 0x3) | 0x8;
@@ -12,15 +12,13 @@ function getRandomStr (){
   });
 }
 /**
-   * 密码加密
-   * @params pwd 待加密密码
-   * @params time 13位时间戳
-   * @returns password 加密结果
-   */
-function encry (pwd, time) {
-  const key = crypto.enc.Utf8.parse(
-    `${time}1zKsCmor4blnFEhiWHfhZLtXFVfwEH3e`
-  );
+ * 密码加密
+ * @params pwd 待加密密码
+ * @params time 13位时间戳
+ * @returns password 加密结果
+ */
+function encry(pwd, time) {
+  const key = crypto.enc.Utf8.parse(`${time}1zKsCmor4blnFEhiWHfhZLtXFVfwEH3e`);
   const iv = crypto.enc.Utf8.parse('20200717');
   return crypto.TripleDES.encrypt(pwd, key, {
     iv: iv,
@@ -28,8 +26,35 @@ function encry (pwd, time) {
     padding: crypto.pad.Pkcs7
   }).toString();
 }
-
-export {
-  encry,
-  getRandomStr
+/**
+ * 时间戳转日期
+ * @param {*} timestampToTime
+ * @param {*} timestamp 时间戳
+ * @type  {*} data 日期 dataTime 日期时间
+ */
+function timestampToTime(timestamp, type) {
+  let date = new Date(
+    timestamp * (timestamp.toString().length === 10 ? 1000 : 1)
+  ); //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+  let Y = date.getFullYear() + '-';
+  let M =
+    (date.getMonth() + 1 < 10
+      ? '0' + (date.getMonth() + 1)
+      : date.getMonth() + 1) + '-';
+  let D =
+    (date.getDate() + 1 < 10 ? '0' + (date.getDate() + 1) : date.getDate()) +
+    ' ';
+  let h =
+    (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+  let m =
+    (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) +
+    ':';
+  let s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+  console.log(`${Y}${M}${D}${h}${m}${s}`);
+  if (type === 'date') {
+    return Y + M + D;
+  } else {
+    return Y + M + D + h + m + s; //时分秒可以根据自己的需求加上
+  }
 }
+export { encry, getRandomStr, timestampToTime };
