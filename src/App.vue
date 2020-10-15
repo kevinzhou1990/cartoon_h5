b3fd7052fc045e53671523f1743675a2973e6c4e
a_can <xiyouji.xiaoyaoyou.lm66@gmail.com>
2020-10-15 17:42:00 +0800

    测试发布

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ export default {
   name: 'App',
   mounted() {
     console.log(this.$route.meta.keepAlive, this.$route.name)
-    console.log('测试地址20201015158')
+    console.log('测试地址202010151741')
   }
 };
 </script>
