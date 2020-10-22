48b346a72d7acbd60cd33c8cdadfa0fb99102b66
AgentK <xiyouji.xiaoyaoyou.clx@gmail.com>
2020-10-22 17:46:27 +0800

    feature:修改dispatch

--- src/store/modules/ranking.js
+++ src/store/modules/ranking.js
@@ -20,7 +20,7 @@ const ranking = {
       return getRankingCate().then(res => {
         if (res.code === 0) {
           commit('UPDATE_RANKING_LIST', res.data.data);
-          // dispatch('getRankingComicsList', res.data.data[0].rank_id);
+          dispatch('getRankingComicsList', res.data.data[0].rank_id);
         }
         return res;
       });
