318bcae20f098f68b8a87ab328a786fbd37176ef
AgentK <xiyouji.xiaoyaoyou.clx@gmail.com>
2020-11-11 22:23:42 +0800

    feature：问题反馈，帮助中心页面

--- src/views/feedback/index.vue
+++ src/views/feedback/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <div class="feedback">
+    <z-m-header title-text="问题反馈" hasBorder>
+      <div slot="right" class="header-right">
+        dasdfsd
+        <!--        <span v-if="showComicsLink">漫画详情</span>-->
+      </div>
+      <!--      <div slot="right" class="title-right" @click="commitQa">提交</div>-->
+    </z-m-header>
+  </div>
 </template>
 
 <script>
-export default {};
+import ZMHeader from '@/common/components/ZMHeader';
+export default {
+  data() {
+    return {};
+  },
+  components: { ZMHeader },
+  methods: {
+    commitQa() {}
+  }
+};
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
