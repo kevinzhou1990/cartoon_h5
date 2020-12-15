7182de5957518d7e20804a4f75b48eb856b62cf9
AgentK <xiyouji.xiaoyaoyou.clx@gmail.com>
2020-12-15 17:34:09 +0800

    fixbug:修复14297，已经点赞的评论，点赞图标未高亮问题

--- src/views/detail/components/ZMDetailRemark.vue
+++ src/views/detail/components/ZMDetailRemark.vue
@@ -9,23 +9,23 @@
       <span style="font-size: 18px;">漫画评论区</span>
       <span class="remark-title-num">{{ remarkCount }}</span>
     </div>
-    <div
-        class="remark-content zm-b-radius"
-        v-if="commentNum"
-        @click.stop="handleClickInfo"
-    >
-        <div class="remark-content-top">
-          <img :src="userIcon" class="remark-content-top-img">
-          <span style="padding: 0 16px; white-space: nowrap;">{{ remarkData.nickname || '--' }}</span>
-          <span class="remark-content-top-time">{{ remarkData.created_at_text }}</span>
-          <div style="position: absolute; right: 12px; top: 16px; height: 24px;">
-            <img class="remark-content-top-like" src="../images/glike.png" alt="">
-            <span class="remark-content-top-num">{{ remarkData.praise_num_text }}</span>
-          </div>
+    <div class="remark-content zm-b-radius" v-if="commentNum" @click.stop="handleClickInfo">
+      <div class="remark-content-top">
+        <img :src="userIcon" class="remark-content-top-img" />
+        <span style="padding: 0 16px; white-space: nowrap;">{{ remarkData.nickname || '--' }}</span>
+        <span class="remark-content-top-time">{{ remarkData.created_at_text }}</span>
+        <div style="position: absolute; right: 12px; top: 16px; height: 24px;">
+          <svg-icon
+            :icon-class="remarkData.has_praise ? 'like_bb' : 'like_ba'"
+            size="small"
+            style="margin-top:-1px"
+          />
+          <span class="remark-content-top-num">{{ remarkData.praise_num_text }}</span>
         </div>
-        <div class="remark-content-text">
-          {{ remarkData.content }}
-        </div>
+      </div>
+      <div class="remark-content-text">
+        {{ remarkData.content }}
+      </div>
     </div>
     <div class="no-remark" v-else>
       <div class="no-remark-text">发表首条评论，沙发就是你的～</div>
@@ -35,11 +35,12 @@
 </template>
 
 <script>
-import { timestampToTime } from 'lib/utils/index'
+import { timestampToTime } from 'lib/utils/index';
+import SvgIcon from '@/common/components/svg';
 export default {
   name: 'ZMDetailRemark',
   props: {
-	  commentNum: {
+    commentNum: {
       type: Number,
       default: 0
     },
@@ -48,47 +49,52 @@ export default {
       default: () => {}
     }
   },
+  components: { SvgIcon },
   computed: {
-	  updateTime() {
-		  return timestampToTime(this.remarkData.created_at, 'date')
-	  },
-	  remarkCount() {
+    updateTime() {
+      return timestampToTime(this.remarkData.created_at, 'date');
+    },
+    remarkCount() {
       if (this.commentNum > 999) {
-        return `评论区999+`
+        return `评论区999+`;
       } else if (this.commentNum === 0) {
-        return '暂无评论'
+        return '暂无评论';
       } else {
-        return `评论区${this.commentNum}`
+        return `评论区${this.commentNum}`;
       }
-	  }
+    }
   },
   data() {
     return {
       isRemarkData: false,
       userIcon: require('../images/default-icon.png')
-    }
+    };
   },
   created() {
     this.$nextTick(() => {
-      this.userIcon = this.remarkData.avatar || require('../images/default-icon.png')
-    })
+      this.userIcon = this.remarkData.avatar || require('../images/default-icon.png');
+    });
   },
   methods: {
-	  /**
-	   * @info: 去评论详情
-	   * @author: PengGeng
-	   * @date: 9/16/20-10:32 上午
+    /**
+     * @info: 去评论详情
+     * @author: PengGeng
+     * @date: 9/16/20-10:32 上午
      * @params remarkType 0 评论  1 评分
-	   */
-	  handleClickInfo() {
-      const cartoonId = (this.$store.state.reader && this.$store.state.reader.comic && this.$store.state.reader.comic.cartoon_id) || this.$route.query.cartoon_id
+     */
+    handleClickInfo() {
+      const cartoonId =
+        (this.$store.state.reader &&
+          this.$store.state.reader.comic &&
+          this.$store.state.reader.comic.cartoon_id) ||
+        this.$route.query.cartoon_id;
       this.$router.push({
         path: '/comments',
         query: {
-	        cartoonId,
-	        remarkType: 0
+          cartoonId,
+          remarkType: 0
         }
-      })
+      });
     },
     /**
      * @info: 点击我要评论
@@ -96,21 +102,21 @@ export default {
      * @date: 8/12/20-7:04 下午
      */
     handleClickRemark() {
-	    this.$router.push('/download')
-      console.log('click remark btn go to remark......')
+      this.$router.push('/download');
+      console.log('click remark btn go to remark......');
     }
   }
-}
+};
 </script>
 
 <style scoped lang="scss">
-  $font-color: #BBBBBB !default;
-  @mixin font10px($font-color) {
-    color: $font-color;
-    font-size: 12px;
-    /*transform: scale(0.83);*/
-    /*-webkit-transform-origin-x: 0;*/
-  }
+$font-color: #bbbbbb !default;
+@mixin font10px($font-color) {
+  color: $font-color;
+  font-size: 12px;
+  /*transform: scale(0.83);*/
+  /*-webkit-transform-origin-x: 0;*/
+}
 .main-remark {
   position: relative;
   font-size: 12px;
@@ -124,7 +130,7 @@ export default {
     &-num {
       position: absolute;
       right: 12px;
-      color: #BBBBBB;
+      color: #bbbbbb;
       font-size: 12px;
       line-height: 26px;
       /*transform: scale(0.83);*/
@@ -136,7 +142,7 @@ export default {
         margin-left: 4px;
         width: 16px;
         height: 16px;
-        background: url("../../../assets/img/more.png") no-repeat;
+        background: url('../../../assets/img/more.png') no-repeat;
         background-size: 100%;
         vertical-align: middle;
       }
@@ -158,7 +164,7 @@ export default {
       }
       &-time {
         white-space: nowrap;
-        @include font10px(#BBBBBB)
+        @include font10px(#bbbbbb);
       }
       &-like {
         display: inline-block;
@@ -168,7 +174,7 @@ export default {
         line-height: 24px;
       }
       &-num {
-        @include font10px(#BBBBBB);
+        @include font10px(#bbbbbb);
         line-height: 24px;
         /*padding-left: 76px;*/
       }
@@ -180,7 +186,7 @@ export default {
   }
   .no-remark {
     font-size: 12px;
-    color: #BBBBBB;
+    color: #bbbbbb;
     letter-spacing: 0;
     text-align: center;
     &-text {
@@ -190,12 +196,12 @@ export default {
       display: inline-block;
       width: 160px;
       height: 44px;
-      border: 1px solid #12E079;
+      border: 1px solid #12e079;
       border-radius: 22px;
       font-size: 14px;
       line-height: 44px;
       align-content: center;
-      color: #12E079;
+      color: #12e079;
       letter-spacing: 0;
       text-align: center;
       &:active {
