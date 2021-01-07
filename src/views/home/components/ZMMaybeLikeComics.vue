fcd07bdd3099fb178f5687fcd6dfa975dc470158
AgentK <xiyouji.xiaoyaoyou.clx@gmail.com>
2021-01-07 15:33:11 +0800

    fixbug:15373

--- src/views/home/components/ZMMaybeLikeComics.vue
+++ src/views/home/components/ZMMaybeLikeComics.vue
@@ -1,59 +1,67 @@
-/**
-* @info: ZM-home-MaybeLike
-* @author: PengGeng
-* @date: 8/5/20-7:19 下午
-*/
+/** * @info: ZM-home-MaybeLike * @author: PengGeng * @date: 8/5/20-7:19 下午 */
 <template>
   <div class="maybe-main">
     <z-m-home-label :label-name="labelName" :rec-id="recId"></z-m-home-label>
     <div
-        class="maybe-main-content"
-        v-for="(item, index) in maybeLikeComicsList"
-        :key="item.cartoon_id"
-        @click="handleZMInfo(item.cartoon_id, 2, recId)"
+      class="maybe-main-content"
+      v-for="(item, index) in maybeLikeComicsList"
+      :key="item.cartoon_id"
+      @click="handleZMInfo(item.cartoon_id, 2, recId)"
     >
       <div class="maybe-main-content-item">
-        <div class="maybe-main-content-item-img"
-             :style="{ background: 'url('+item.cover+'), url(' + defaultLCover + ') no-repeat', 'background-size': '100%'}"></div>
+        <div
+          class="maybe-main-content-item-img"
+          :style="{
+            background: 'url(' + item.cover + '), url(' + defaultLCover + ') no-repeat',
+            'background-size': '100%'
+          }"
+        ></div>
         <div class="maybe-main-content-item-desc">
           <div class="maybe-main-content-item-desc-title">
-            <span class="maybe-main-content-item-desc-title-text">{{ item.title || '--'}}</span>
+            <span class="maybe-main-content-item-desc-title-text">{{ item.title || '--' }}</span>
             <div class="maybe-main-content-item-desc-title-c" @click.stop="handleClickCollect">
               <img
-                  class="maybe-main-content-item-desc-title-c-img"
-                  :src="isCollectFlag ? ba_img : bb_img">
+                class="maybe-main-content-item-desc-title-c-img"
+                :src="isCollectFlag ? bb_img : ba_img"
+              />
               <span :class="{ 'f-c-g': !isCollectFlag }">收藏</span>
             </div>
           </div>
           <div class="maybe-main-content-item-desc-chapter">
-            <span class="maybe-main-content-item-desc-chapter-author">{{ item.author | authorFormate }}</span>
+            <span class="maybe-main-content-item-desc-chapter-author">{{
+              item.author | authorFormate
+            }}</span>
             <span class="p-l10">{{ item.publish_status || '--' }}</span>
           </div>
           <div class="maybe-main-content-item-desc-label">
-            <span class="s-border"
-                  :class="{'m-l8' : index> 0}"
-                  v-for="(tagItem, index) in item.tag"
-                  :key="index"
+            <span
+              class="s-border"
+              :class="{ 'm-l8': index > 0 }"
+              v-for="(tagItem, index) in item.tag"
+              :key="index"
             >
               {{ tagItem }}
             </span>
           </div>
           <span class="maybe-main-content-item-desc-content">
-            {{ item.intro || '暂无'}}
+            {{ item.intro || '暂无' }}
           </span>
         </div>
       </div>
-      <div class="maybe-main-content-item-line zm-b-t" v-if="index < maybeLikeComicsList.length - 1"></div>
+      <div
+        class="maybe-main-content-item-line zm-b-t"
+        v-if="index < maybeLikeComicsList.length - 1"
+      ></div>
     </div>
     <z-m-home-a-d v-if="ad_list.length" :ad-data="ad_list"></z-m-home-a-d>
   </div>
 </template>
 
 <script>
-import ZMHomeLabel from './ZMHomeLabel'
-import '@/common/filters/home'
-import myMixins from '@/common/mixin/myMixins'
-import ZMHomeAD from './ZMHomeAD'
+import ZMHomeLabel from './ZMHomeLabel';
+import '@/common/filters/home';
+import myMixins from '@/common/mixin/myMixins';
+import ZMHomeAD from './ZMHomeAD';
 export default {
   name: 'ZMPossibale',
   mixins: [myMixins],
@@ -73,17 +81,17 @@ export default {
       nums: [1, 2, 3],
       recId: 6,
       ad_list: []
-    }
+    };
   },
   components: {
     ZMHomeLabel,
     ZMHomeAD
   },
   created() {
-    this.labelName = this.maybeLikeComics.name
-    this.recId = this.maybeLikeComics.rec_id || 6
-    this.maybeLikeComicsList = this.maybeLikeComics.cartoon_list
-    this.ad_list = this.maybeLikeComics.ad_list
+    this.labelName = this.maybeLikeComics.name;
+    this.recId = this.maybeLikeComics.rec_id || 6;
+    this.maybeLikeComicsList = this.maybeLikeComics.cartoon_list;
+    this.ad_list = this.maybeLikeComics.ad_list;
   },
   methods: {
     /**
@@ -92,30 +100,30 @@ export default {
      * @date: 8/5/20-7:25 下午
      */
     handleClickMybeLike() {
-      console.log('go to maybe like more info')
+      console.log('go to maybe like more info');
     },
-	  /**
-	   * @info: 点击了收藏
-	   * @author: PengGeng
-	   * @date: 10/26/20-10:25 上午
-	   */
-	  handleClickCollect() {
+    /**
+     * @info: 点击了收藏
+     * @author: PengGeng
+     * @date: 10/26/20-10:25 上午
+     */
+    handleClickCollect() {
       // 是否确认收藏
-		  // this.isCollectFlag = !this.isCollectFlag
-      this.$router.push('/download')
+      // this.isCollectFlag = !this.isCollectFlag
+      this.$router.push('/download');
     }
   }
-}
+};
 </script>
 <style scoped lang="scss">
-  $maybe-fontSize: 18px;
-  $maybe-color: #222222;
-  $banner-font-color: #FFFFFF;
-  $title-fontSize: 14px;
-  $chapter-color: #BBBBBB;
-  $chapter-fontSize: 12px;
-  $content-fontSize: 12px;
-  $content-color: #999999;
+$maybe-fontSize: 18px;
+$maybe-color: #222222;
+$banner-font-color: #ffffff;
+$title-fontSize: 14px;
+$chapter-color: #bbbbbb;
+$chapter-fontSize: 12px;
+$content-fontSize: 12px;
+$content-color: #999999;
 .f-c-g {
   color: $chapter-color;
 }
@@ -144,7 +152,7 @@ export default {
         width: 90px;
         height: 120px;
         border-radius: 4px;
-        background: url("../../../assets/img/defaultBook.png");
+        background: url('../../../assets/img/defaultBook.png');
         background-size: cover;
       }
       &-desc {
@@ -167,7 +175,7 @@ export default {
           &-c {
             display: flex;
             justify-content: space-between;
-            color: #12E079;
+            color: #12e079;
             font-size: $content-fontSize;
             &-img {
               width: 16px;
@@ -218,7 +226,7 @@ export default {
         }
       }
       &-line {
-        display:flex;
+        display: flex;
         box-sizing: border-box;
         font-weight: normal;
         opacity: 0.8;
