9e043d04a96f9118f160f15833bc5f59a967745e
AgentK <xiyouji.xiaoyaoyou.clx@gmail.com>
2021-01-14 18:10:19 +0800

    fixbug：一直请求auth接口的问题

--- src/lib/utils/const.js
+++ src/lib/utils/const.js
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 export const VERSION = '1.0.0'; // 版本号
 export const PLATFORM = 2; // APP类型，1-PC web 2-H5 web 3-IOS APP 4-Android APP  5-后台管理web
 export const CHANNEL = 0; // APP渠道，目前固定为0
-export const DEVICE_ID = uuidv4(); // 设备唯一标识
+export const DEVICE_ID = uuidv4(); //'562e30b9-405a-4536-9da5-a370059e1557'; // 设备唯一标识
 export const DEVICE_MODEL = '网页设备'; // 设备型号
 export const CID = ''; // 推送标识
 export const INSTALL_TIME = ''; // APP安装时间，APP必须
