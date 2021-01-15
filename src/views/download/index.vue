14aa838d30f2b3f388049f2a2f889db0b77367f1
AgentK <xiyouji.xiaoyaoyou.clx@gmail.com>
2021-01-15 19:20:39 +0800

    fixbug:修复下载页面报错问题

--- src/views/download/index.vue
+++ src/views/download/index.vue
@@ -3,20 +3,23 @@
 </template>
 
 <script>
-const iosUrl = 'https://itunes.apple.com/cn/app/'; // todo 待定
-const androidUrl = location.href.indexOf('localhost') > -1 ? 'http://10.1.15.98:9501/api/android/down' : `${location.origin}/api/android/down`
 export default {
   name: 'download',
   data() {
     return {
       isIos: false,
-      isAndroid: false
+      isAndroid: false,
+      iosUrl: 'https://itunes.apple.com/cn/app/',
+      androidUrl: 'http://10.1.15.98:9501/api/android/down'
     };
   },
   mounted() {
     this.isIos = !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
     this.isAndroid =
       navigator.userAgent.indexOf('Android') > -1 || navigator.userAgent.indexOf('Adr') > -1;
+    if (location.host.indexOf('localhost') < 0) {
+      this.androidUrl = `${location.origin}/api/android/down`;
+    }
   },
   methods: {
     createIframe() {
@@ -53,20 +56,20 @@ export default {
           if (timeOutDateTime - loadDateTime < 2200) {
             // var t = document.createEvent('HTMLEvents'); // 返回新创建的 Event 对象，具有指定的类型。
             // t.initEvent('click', !1, !1) // 初始化新事件对象的属性
-            window.location.href = iosUrl;
+            window.location.href = this.iosUrl;
           }
         }, 2000);
       } else {
         // 判断是否是android
         if (isChrome) {
           // chrome浏览器用iframe打不开得直接去打开，算一个坑
-          window.location.href = url
+          window.location.href = url;
         } else {
           //抛出你的scheme
           openIframe.src = url;
         }
-        setTimeout(function () {
-          window.location.href = androidUrl;
+        setTimeout(function() {
+          window.location.href = this.androidUrl;
         }, 500);
       }
     }
